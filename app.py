91961044abff2ec729594e72f882b701e0072d38
Aksanvb <sanjayanand060@gmail.com>
2025-11-10 09:55:43 +0530

    Fix SMS API key syntax

--- app.py
+++ app.py
@@ -87,19 +87,20 @@ def read_fast2sms_key() -> str:
 
 def send_sms(mobile, message):
     try:
-        url = f"https://2factor.in/API/V1/"682fcd54-bde7-11f0-bdde-0200cd936042"/ADDON_SERVICES/SEND/TSMS"
+        url = f"https://2factor.in/API/V1/{"682fcd54-bde7-11f0-bdde-0200cd936042"}/ADDON_SERVICES/SEND/TSMS"
         
         payload = {
-            "From": "SANJAY",   # 6 letters ONLY
+            "From": "SANJAY",     # 6-letter sender name (UPPERCASE)
             "To": mobile,
             "Msg": message
         }
         
         response = requests.post(url, data=payload)
-        print("SMS Sent →", response.text, flush=True)
-        
+        print("SMS Response →", response.text)
+
     except Exception as e:
-        print("SMS Error:", e, flush=True)
+        print("SMS Error:", e)
+
 
 
 
